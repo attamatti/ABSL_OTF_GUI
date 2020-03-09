959c6193f7610b924032ca855401958198e524df
Matthew Iadanza <fbsmi@fbsabsl-wkstn06.leeds.ac.uk>
2020-03-09 10:41:13 +0000

    bug fixes

--- OTF_GUI_hardkill.py
+++ OTF_GUI_hardkill.py
@@ -12,14 +12,15 @@ PIDs = []
 for i in running:
 	line = i.split()
 	if 'new_OTF' in i:
-	PIDs.append(line[1])
+		PIDs.append(line[1])
 
 #### inform user
-print('KILL: Found {0} instances of the script running'.format(len(PIDS))
-print('KILL: Killing PIDs: {0}'.format(PIDs))
+print('KILL: Found {0} instances of the script running'.format(len(PIDs)))
+
 
 #### kill any running processes
 if len(PIDs) > 0:
+	print('KILL: Killing PIDs: {0}'.format(PIDs))
 	for i in PIDs:
 		subprocess.call('kill -9 {0}'.format(i),shell=True)
 		print('KILL: Successfully killed {0}'.format(i))
@@ -28,7 +29,7 @@ if len(PIDs) > 0:
 subprocess.call('pkill rsync',shell=True)
 
 #### remove OTFFT_running file if it exists
-if os.path.isfile(OTFFT_running) == True:
+if os.path.isfile('OTFFT_running') == True:
 	subprocess.call('rm OTFFT_running',shell=True)
 	print('KILL: Removed OTF')
 
@@ -41,14 +42,15 @@ for i in relionfiles:
 #### remove any symlinks - but ask user first
 links = glob.glob('Raw_data/*')
 print('KILL: There are {0} file links in Raw_data'.format(len(links)))
-remove = input('KILL: Remove them (y/n): ')
+if len(links) > 0:
+	remove = input('KILL: Remove them (y/n): ')
 
-if remove in ['y','Y','Yes','yes','YES']:
-	for i in links:
-		subprocess.call('unlink {0}'.format(os.path.abspath(i)),shell=True)
-	print('KILL: Removed {0} symlinks'.format(len(links)))
-else:
-	print('KILL: Leaving symlinks in place')
+	if remove in ['y','Y','Yes','yes','YES']:
+		for i in links:
+			subprocess.call('unlink {0}'.format(os.path.abspath(i)),shell=True)
+		print('KILL: Removed {0} symlinks'.format(len(links)))
+	else:
+		print('KILL: Leaving symlinks in place')
 
 #### my work here is done
 print('KILL: Finished')
